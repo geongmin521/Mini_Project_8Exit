c39e503198ed5ece01fe8646ffa9fbaf6e95a5c8
semin0723 <45254558+semin0723@users.noreply.github.com>
2024-05-17 11:32:35 +0900

    Add : ObjectHeader Group
    씬에 배치할 오브젝트가 많아짐에 따라 오브젝트의 헤더를 모아두는 헤더를 만들어 분리합니다.

---
+++ Win32Game/InGameObjectHeader.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "BackGround.h"
+#include "Player.h"
+#include "Spider1.h"
+#include "Spider2.h"
+#include "SunFlower.h"
+#include "TextBox.h"
+#include "Snake.h"
+#include "ScareCrow.h"
+#include "ScareCrow_Search.h"
