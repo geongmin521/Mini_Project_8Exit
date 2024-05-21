6212aa016a235fc1cc0dd5f268a1ff17fe8bdd11
YesChan2 <hyc7685@naver.com>
2024-05-21 14:43:15 +0900

    add_dark_예찬

--- Win32Game/DarkChange.cpp
+++ Win32Game/DarkChange.cpp
@@ -2,41 +2,98 @@
 #include "TimeSystem.h"
 #include "ResourceManager.h"
 #include "InputSystem.h"
-DarkChange::DarkChange() :_MyTex(nullptr), _Min(1.0f), _Down(1.0f / 60.0f)
+#include "Camera.h"
+#include <random>
+#include <gdiplus.h>
+
+#pragma comment(lib, "gdiplus.lib")
+DarkChange::DarkChange() :_MyTex(nullptr), _Min(1.0f), _Down(1.0f / 60.0f), _cachedBitmap(nullptr), _graphics(nullptr)
 {
-	_MyTex = resourceManager->GetTexture(L"DarkChange", L"Image\\DarkChange.png");
+	_MyTex = resourceManager->GetTexture(L"DarkChange", L"Image\\Dark_Change.bmp");
+	GdiplusStartupInput gdiplusStartupInput;
+	ULONG_PTR gdiplusToken;
+	GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+
+	// Initialize graphics object
+	HDC hdc = GetDC(NULL);
+	_graphics = new Gdiplus::Graphics(hdc);
+
+	// Create the cached bitmap
+	CreateCachedBitmap();
+
+	ReleaseDC(NULL, hdc);
 }
 
 DarkChange::~DarkChange()
 {
+
 }
 
 
 void DarkChange::Update()
 {
-	if (inputSystem->isKey('V'))
-	{
-		Min();
-	}
+	
+	
 }
 
 void DarkChange::Render()
 {
 	Vector3 renderPosition = camera->GetRenderPos(GameObject::GetLocation());
 
-	Gdiplus::Graphics g(renderSystem->_backDC);
-	
-	g.DrawImage(_MyTex->GetImage(),
-		(int)renderPosition._x - (int)_MyTex->GetImage()->GetWidth() / 2,
-		(int)renderPosition._y - (int)_MyTex->GetImage()->GetHeight() / 2,
-		(int)_MyTex->GetImage()->GetWidth(), (int)_MyTex->GetImage()->GetHeight()
-	);
+	if (_cachedBitmap)
+	{
+		int startX = static_cast<int>(renderPosition._x) - _cachedBitmapWidth / 2;
+		int startY = static_cast<int>(renderPosition._y) - _cachedBitmapHeight / 2;
 
+		_graphics->DrawCachedBitmap(_cachedBitmap, startX, startY);
+	}
 }
+
 void DarkChange::Min()
 {
 	_Min -= _Down * timeManager->GetDeltaTime();
 
 	if (_Min < 0.0f)
 		_Min = 0.0f;
+}
+//보라색 빼는 함수
+
+
+void DarkChange::die()
+{
+
+}
+//플레이어가 죽었을 때 이미지 작아짐
+
+//플레이어 죽는 연결 함수
+
+//
+void DarkChange::CreateCachedBitmap()
+{
+	int width = _MyTex->Width();
+	int height = _MyTex->Height();
+
+	Bitmap tmpBit(width, height, PixelFormat32bppARGB);
+	Graphics* pMemGraphics = Graphics::FromImage(&tmpBit);
+
+	Color transColor(255, 0, 255); // 보라색을 투명하게
+	ColorMap colorMap;
+	colorMap.oldColor = transColor;
+	colorMap.newColor = Color(0, 0, 0, 0); // 투명한 색으로 대체
+
+	ImageAttributes imgAttr;
+	imgAttr.SetRemapTable(1, &colorMap, ColorAdjustTypeBitmap);
+	imgAttr.SetColorKey(transColor, transColor, ColorAdjustTypeBitmap);
+
+	pMemGraphics->DrawImage(_MyTex->GetImage(),
+		Rect(0, 0, width, height),
+		0, 0, width, height, UnitPixel, &imgAttr);
+
+	delete pMemGraphics;
+
+	_cachedBitmap = new CachedBitmap(&tmpBit, _graphics);
+
+	// Store the dimensions of the cached bitmap
+	_cachedBitmapWidth = width;
+	_cachedBitmapHeight = height;
 }
--- Win32Game/DarkChange.h
+++ Win32Game/DarkChange.h
@@ -2,6 +2,8 @@
 #include "ResourceManager.h" 
 #include "TimeSystem.h"
 #include "RenderSystem.h"
+#include <windows.h>
+#include <gdiplus.h>
 class DarkChange : public GameObject
 {
 public:
@@ -10,14 +12,24 @@ public:
 	void Update();
 	void Render();
 	void Min();
+	void WM_Paint();
+	void die();
+	
 private:
-
+	void CreateCachedBitmap();
 	float _Min;
 	float _Down;
 	float _Max;
 	Texture* _MyTex;
-
+	Gdiplus::Bitmap* _MyTex;
 	ResourceManager* ResourceManager;
 	TimeManager* TimeManager;
+	HBITMAP _hBitmap;
+	HDC _hMemDC;
+	Gdiplus::CachedBitmap* _cachedBitmap;
+	Gdiplus::Graphics* _graphics;
+	int _cachedBitmapWidth; // Cached bitmap width
+	int _cachedBitmapHeight; // Cached bitmap height
+	ULONG_PTR gdiplusToken; // GDI+ ÅäÅ«
 };
 
